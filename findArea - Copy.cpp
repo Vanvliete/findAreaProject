d64471b7a6f34fb894583a4252fd7253c1304985
Erik Van Vliet <46632224+Vanvliete@users.noreply.github.com>
2020-09-14 18:15:44 -0400

    Add files via upload

---
+++ findArea - Copy.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+
+
+	double length = 0, width = 0, area = 0;
+
+	
+
+	cout << "Enter length" << endl;
+
+	cin >> length;
+
+	cout << endl;
+
+	cout << "Enter width" << endl;
+
+	cin >> width;
+
+	area = length * width;
+
+	cout << "Area = " << area << endl;
+
+	system("pause");
+
+
+
+
+
+
+
+
+
+}
